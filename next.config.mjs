7cae6d3c46238b6ec62721e6e87b7e3505a037a5
Nourgo Fantiele Coulibaly <159635876+nourgocoulibaly@users.noreply.github.com>
2024-07-28 19:23:21 +0000

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
 
 	images: {
 		domains: ["pbs.twimg.com"],
+		unoptimized: true,
 	},
 
 	reactStrictMode: true,
--- src/app/adminRequests/_components/adminRequestsBeta.tsx
+++ src/app/adminRequests/_components/adminRequestsBeta.tsx
@@ -4,9 +4,12 @@ import { db } from "@/lib/firebaseConfig";
 import { collection, deleteDoc, doc, getDocs, updateDoc } from "firebase/firestore";
 import { useEffect, useRef, useState } from "react";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
+
 import { Button } from "@/components/ui/button";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 
 import {
 	Card,
@@ -43,7 +46,6 @@ import {
 	File, History, ListFilter
 } from 'lucide-react';
 
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 
@@ -53,100 +55,105 @@ const AdminRequestsBeta = () => {
   const [filteredRequests, setFilteredRequests] = useState<any[]>([]);
 	const router = useRouter();
 	const [activeTab, setActiveTab] = useState('all');
+  // const [defaultTab, setDefaultTab] = useState<string>('all');
 
   useEffect(() => {
     const fetchRequests = async () => {
-      try {
-        const querySnapshot = await getDocs(collection(db, "userRequests"));
-        const requestsData: any[] = [];
+      const querySnapshot = await getDocs(collection(db, "userRequests"));
+      const requestsData: any[] = [];
+      querySnapshot.forEach((doc) => {
+        const userData = doc.data();
+        const userName = userData.userName;
+        requestsData.push({ id: doc.id, ...userData, userName });
+      });
 
-        querySnapshot.forEach((doc) => {
-          const userData = doc.data();
-          const userName = userData.userName;
-          const createdAt = userData.createdAt; // Garder le code temporel
-          const updatedAt = userData.updatedAt || null; // Garder le code temporel
-          requestsData.push({ id: doc.id, ...userData, userName, createdAt, updatedAt });
-        });
+			  // Tri des requêtes par ordre décroissant de date
+				requestsData.sort((a, b) => {
+					const dateA = a.createdAt?.toDate() || a.updatedAt?.toDate();
+					const dateB = b.createdAt?.toDate() || b.updatedAt?.toDate();
+					if (dateA && dateB) {
+						return dateB.getTime() - dateA.getTime();
+					}
+					return 0;
+				});
 
-        requestsData.sort((a, b) => {
-          const dateA = a.createdAt || a.updatedAt;
-          const dateB = b.createdAt || b.updatedAt;
-          if (dateA && dateB) {
-            return dateB - dateA; // Comparer les codes temporels directement
-          }
-          return 0;
-        });
-
-        setRequests(requestsData);
-        setFilteredRequests(requestsData);
-      } catch (error) {
-        console.error("Erreur lors de la récupération des requêtes :", error);
-      }
+      setRequests(requestsData);
+      setFilteredRequests(requestsData); // Initialiser avec toutes les requêtes
     };
 
     fetchRequests();
+
   }, []);
 
+  
   const handleMarkAs = async (id: string, status: string) => {
-    try {
-      const requestDoc = doc(db, "userRequests", id);
-      await updateDoc(requestDoc, { status, updatedAt: new Date().getTime() }); // Mise à jour avec le code temporel
-      setRequests(requests.map(request => request.id === id ? { ...request, status, updatedAt: new Date().getTime() } : request));
-    } catch (error) {
-      console.error("Erreur lors de la mise à jour de la requête :", error);
-    }
-  };
+		const requestDoc = doc(db, "userRequests", id);
+		await updateDoc(requestDoc, { status });
+		// Mettre à jour localement le statut de la requête
+		setRequests(requests.map(request => request.id === id ? { ...request, status } : request));
+	};
 
-  const handleEditRequest = (id: string) => {
+	const handleEditRequest = (id: string) => {
+    const requestToEdit = requests.find(request => request.id === id);
+
+
+    console.log("Modifier la requête :", requestToEdit);
+    console.log(requestToEdit);
+    console.log("Chemin request/page.tsx");
+
+    // Rediriger vers la page request/page.tsx avec l'id de la requête
     router.push(`/adminRequests/request/${id}`);
-  };
+    // router.push(`/adminRequests/request/[id]`);
+    // router.push(`/adminRequests/request`);
+};
 
   const handleDeleteRequest = async (id: string) => {
-    try {
-      const requestDoc = doc(db, "userRequests", id);
-      await deleteDoc(requestDoc);
-    } catch (error) {
-      console.error("Erreur lors de la suppression de la requête :", error);
-    }
+    const requestDoc = doc(db, "userRequests", id);
+    await deleteDoc(requestDoc);
   };
+
+  // Logique de filtrage des requêtes en fonction du temps
+
 
   const filterRequestsByTime = (time: string) => {
     let filteredRequests = [];
 
     if (time === 'week') {
         filteredRequests = requests.filter(request => {
-            const requestDate = new Date(request.createdAt);
+            const requestDate = request.createdAt?.toDate();
             const currentDate = new Date();
             const oneWeekAgo = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000);
-            return requestDate.getTime() >= oneWeekAgo.getTime() && requestDate.getTime() <= currentDate.getTime();
+            return requestDate && requestDate.getTime() >= oneWeekAgo.getTime() && requestDate.getTime() <= currentDate.getTime();
         });
         console.log('🤖Filtre Semaine');
     } else if (time === 'month') {
 			filteredRequests = requests.filter(request => {
-				const requestDate = new Date(request.createdAt);
+				const requestDate = request.createdAt?.toDate();
             const currentDate = new Date();
             const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
             const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
-            return requestDate.getTime() >= firstDayOfMonth.getTime() && requestDate.getTime() <= lastDayOfMonth.getTime();
+            return requestDate && requestDate.getTime() >= firstDayOfMonth.getTime() && requestDate.getTime() <= lastDayOfMonth.getTime();
         });
         console.log('🤖Filtre Mois');
     } else if (time === 'year') {
         filteredRequests = requests.filter(request => {
-            const requestDate = new Date(request.createdAt || request.updatedAt);
+            const requestDate = request.createdAt?.toDate() || request.updatedAt?.toDate();
             const currentDate = new Date();
             const firstDayOfYear = new Date(currentDate.getFullYear(), 0, 1);
             const lastDayOfYear = new Date(currentDate.getFullYear(), 11, 31);
-            return requestDate.getTime() >= firstDayOfYear.getTime() && requestDate.getTime() <= lastDayOfYear.getTime();
+            return requestDate && requestDate.getTime() >= firstDayOfYear.getTime() && requestDate.getTime() <= lastDayOfYear.getTime();
         });
         console.log('🤖Filtre Annee');
     } else if (time === 'all') {
+			// Afficher toutes les requêtes sans exception
 			filteredRequests = requests;
 			console.log('🤖Filtre Tout');
 		}
 
+    // Tri des requêtes par ordre décroissant de date
     filteredRequests.sort((a, b) => {
-      const dateA = new Date(a.createdAt || a.updatedAt);
-      const dateB = new Date(b.createdAt || b.updatedAt);
+      const dateA = a.createdAt?.toDate() || a.updatedAt?.toDate();
+      const dateB = b.createdAt?.toDate() || b.updatedAt?.toDate();
 
       if (dateA && dateB) {
         return dateB.getTime() - dateA.getTime();
@@ -158,7 +165,15 @@ const AdminRequestsBeta = () => {
     setFilteredRequests(filteredRequests);
 };
 
-  const filterRequestsByTimeRef = useRef(filterRequestsByTime);
+  const filterRequestsByTimeRef = useRef(filterRequestsByTime); // Utiliser une référence à la fonction
+
+  // useEffect(() => {
+  //   if (requests.length > 0) {
+  //     filterRequestsByTimeRef.current(defaultTab); // Appeler la fonction via la référence
+  //   }
+  // }, [requests, defaultTab, filterRequestsByTimeRef]);
+
+
 
   return (
     <div className='flex min-h-screen w-full flex-col bg-muted/40 '>
@@ -174,9 +189,9 @@ const AdminRequestsBeta = () => {
 								</CardDescription>
 							</CardHeader>
 							<CardFooter>
-							<Link href='/adminRequests/requestForm'>
+							<a href='/adminRequests/requestForm'>
     <Button>Créer une nouvelle commande</Button>
-</Link>
+</a>
 							</CardFooter>
 						</Card>
 						<Card x-chunk='dashboard-05-chunk-1'>
@@ -254,7 +269,7 @@ const AdminRequestsBeta = () => {
 								</DropdownMenu>
 								<DropdownMenu>
 								<DropdownMenuTrigger asChild>
-									<Link href='/adminRequests/history'>
+									<a href='/adminRequests/history'>
 										<Button
 											variant='outline'
 											size='sm'
@@ -263,14 +278,14 @@ const AdminRequestsBeta = () => {
 											<History className='h-3.5 w-3.5' />
 											<span className='sr-only sm:not-sr-only'>Historique</span>
 										</Button>
-										</Link>
+										</a>
 										</DropdownMenuTrigger>
 								</DropdownMenu>
     {/* <DropdownMenuTrigger asChild>
 		<DropdownMenuContent>
-      <Link href='/adminInventory/history' className='h-7 gap-1 text-sm'>
+      <a href='/adminInventory/history' className='h-7 gap-1 text-sm'>
         Historique
-      </Link>
+      </a>
     </DropdownMenuTrigger>
 		</DropdownMenuContent>
   </DropdownMenu> */} 
@@ -315,6 +330,12 @@ const AdminRequestsBeta = () => {
 												<TableRow key={request.id} onClick={() => handleEditRequest(request.id)} 
 												style={{ cursor: 'pointer' }}>
 												{/* <TableRow key={request.id}> */}
+													{/* <TableCell className='hidden sm:table-cell'>
+													<Avatar className='hidden h-9 w-9 sm:flex'>
+                    <AvatarImage src={`/avatars/${request.id}.png`} alt='Avatar' />
+                    <AvatarFallback>{request.userName.substring(0, 2).toUpperCase()}</AvatarFallback>
+                </Avatar>
+                          </TableCell> */}
 													<TableCell className='hidden sm:table-cell'>
                             <div className='font-medium'>{request.userName}</div>
                             <div className='hidden text-sm text-muted-foreground md:inline'>{request.userDirection}</div>
@@ -328,7 +349,7 @@ const AdminRequestsBeta = () => {
 													</Badge>       
                           </TableCell>
                           <TableCell className='hidden sm:table-cell'>
-                          {request.createdAt && request.createdAt.toDate ? request.createdAt.toDate().toLocaleString() : ''}
+                          {request.createdAt?.toDate()?.toLocaleString() || ''}
                             {/* {request.createdAt ? request.createdAt.dates : ''} */}
                           </TableCell>
 													{/* <TableCell>
@@ -383,7 +404,7 @@ const AdminRequestsBeta = () => {
 													</Badge>       
                           </TableCell>
                           <TableCell className='hidden sm:table-cell'>
-                          {request.createdAt && request.createdAt.toDate ? request.createdAt.toDate().toLocaleString() : ''}
+                          {request.createdAt?.toDate()?.toLocaleString() || ''}
                             {/* {request.createdAt ? request.createdAt.dates : ''} */}
                           </TableCell>
 													{/* <TableCell>
@@ -440,64 +461,7 @@ const AdminRequestsBeta = () => {
 													</Badge>       
                           </TableCell>
                           <TableCell className='hidden sm:table-cell'>
-                          {request.createdAt && request.createdAt.toDate ? request.createdAt.toDate().toLocaleString() : ''}
-                            {/* {request.createdAt ? request.createdAt.dates : ''} */}
-                          </TableCell>
-													{/* <TableCell>
-													<Button onClick={() => handleEditRequest(request.id)}><Settings /></Button>													
-													</TableCell> */}
-												</TableRow>
-											))}
-										</TableBody>
-									</Table>
-								</CardContent>
-							</Card>
-						</TabsContent>
-
-            {/* Tabs value Month */}
-						<TabsContent value="month">
-							<Card x-chunk='dashboard-05-chunk-3'>
-								<CardHeader className='px-7'>
-									<CardTitle>Interventions</CardTitle>
-									<CardDescription>
-										Liste des demandes des Interventions
-									</CardDescription>
-								</CardHeader>
-								<CardContent>
-									<Table>
-										<TableHeader>
-											<TableRow>
-												<TableHead>Utilisateur</TableHead>
-												<TableHead className='hidden sm:table-cell'>
-													Nature de l&apos;Intervention
-												</TableHead>
-												<TableHead className='hidden sm:table-cell'>
-													Statut
-												</TableHead>
-												<TableHead className='hidden md:table-cell'>
-													Date
-												</TableHead>
-											</TableRow>
-										</TableHeader>
-										<TableBody>
-										{filteredRequests.map((request) => (
-											<TableRow key={request.id} onClick={() => handleEditRequest(request.id)} 
-											style={{ cursor: 'pointer' }}>
-            {/* <TableRow key={request.id}> */}
-              <TableCell className='hidden sm:table-cell'>
-                            <div className='font-medium'>{request.userName}</div>
-                            <div className='hidden text-sm text-muted-foreground md:inline'>{request.userDirection}</div>
-                          </TableCell>
-                          <TableCell className='hidden sm:table-cell'>
-                            {request.requestContent}
-                          </TableCell>
-                          <TableCell className='hidden sm:table-cell'>
-                          <Badge className='text-xs' variant='secondary'>
-                          {request.requestStatus}
-													</Badge>       
-                          </TableCell>
-                          <TableCell className='hidden sm:table-cell'>
-                          {request.createdAt && request.createdAt.toDate ? request.createdAt.toDate().toLocaleString() : ''}
+                          {request.createdAt?.toDate()?.toLocaleString() || ''}
                             {/* {request.createdAt ? request.createdAt.dates : ''} */}
                           </TableCell>
 													{/* <TableCell>
@@ -554,7 +518,7 @@ const AdminRequestsBeta = () => {
                 </Badge>       
               </TableCell>
               <TableCell className='hidden sm:table-cell'>
-                {request.createdAt && request.createdAt.toDate ? request.createdAt.toDate().toLocaleString() : ''}
+                {request.createdAt?.toDate()?.toLocaleString() || ''}
               </TableCell>
 							{/* <TableCell>
 													<Button onClick={() => handleEditRequest(request.id)}><Settings /></Button>													
@@ -575,7 +539,7 @@ const AdminRequestsBeta = () => {
 						<CardHeader className='flex flex-row items-start bg-muted/50'>
 							<div className='grid gap-0.5'>
 								<CardTitle className='group flex items-center gap-2 text-lg'>
-									Nombre de Requte par Utilisateur
+									Nombre de Requête par Utilisateur
 								</CardTitle>						
 							</div>
 							<div className='ml-auto flex items-center gap-1'>
@@ -614,13 +578,3 @@ const AdminRequestsBeta = () => {
 };
 
 export default AdminRequestsBeta;
-
-
-
-
-
-
-
-
-
-

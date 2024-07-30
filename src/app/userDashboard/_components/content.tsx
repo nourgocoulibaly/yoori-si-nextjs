be2d0532f53ca885b4a4b539adc545c5b15983a1
busy0nourgo <busy0nourgo@gmail.com>
2024-07-30 14:56:11 +0000

    modified

--- src/app/userDashboard/_components/content.tsx
+++ src/app/userDashboard/_components/content.tsx
@@ -50,11 +50,11 @@ export default function UserDashboardContent() {
 				>
 					<div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
 					<h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-						Demande d'Intervention
+						Demande d&apos;Intervention
 					</h3>
 					<div className="text-base !m-0 !p-0 font-normal">
 						<span className="text-slate-500 ">
-						Envoyer une nouvelle demande d'intervention.
+						Envoyer une nouvelle demande d&apos;intervention.
 						</span>
 					</div>
 					<div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />

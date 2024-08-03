542186c71cfa935fe66143d92da65c110c553bd9
Nourgo Fantiele Coulibaly <159635876+nourgocoulibaly@users.noreply.github.com>
2024-08-03 13:20:39 +0000

    new update

--- src/app/adminDashboard/_components/navbar.tsx
+++ src/app/adminDashboard/_components/navbar.tsx
@@ -113,7 +113,7 @@ export default function AdminNavbar({
   >
     <Image
       src={`/YooriLinkLarge.png`}
-      className="w-40 h-auto mr-35"
+      className="w-52 h-auto mr-35"
       alt="Logo Yoori Link"
       loading="lazy"
     />

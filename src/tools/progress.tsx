4ddd38a5391741976d91b4bca82ae87f3e434d41
Nourgo Fantiele Coulibaly <159635876+nourgocoulibaly@users.noreply.github.com>
2024-08-04 09:30:47 +0000

    update ui

--- src/tools/progress.tsx
+++ src/tools/progress.tsx
@@ -1,20 +1,7 @@
 import CircularProgress from '@mui/material/CircularProgress';
-import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
 import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
 import * as React from 'react';
 
-const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
-  height: 10,
-  borderRadius: 5,
-  [`&.${linearProgressClasses.colorPrimary}`]: {
-    backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
-  },
-  [`& .${linearProgressClasses.bar}`]: {
-    borderRadius: 5,
-    backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
-  },
-}));
 
 function GradientCircularProgress() {
   return (
@@ -36,8 +23,6 @@ export default function Progress() {
   return (
     <Stack spacing={2} sx={{ flexGrow: 1 }}>
       <GradientCircularProgress />
-      <br />
-      <BorderLinearProgress variant="determinate" value={50} />
     </Stack>
   );
 }

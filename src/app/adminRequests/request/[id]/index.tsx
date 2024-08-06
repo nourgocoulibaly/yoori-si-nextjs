f3a343c9ab0adeb28d777b055e5879fa214da37d
busy0nourgo <busy0nourgo@gmail.com>
2024-08-06 12:05:13 +0000

    new update

--- src/app/adminRequests/request/[id]/index.tsx
+++ src/app/adminRequests/request/[id]/index.tsx
@@ -259,7 +259,7 @@ const RequestPage = ({ params, data }: { params: { id: string }; data: any }) =>
 		requestDomain: '',
 		requestStatus: '',
 		createdAt: { toDate: () => Date },
-		interventionDate: '',
+		interventionDate: { toDate: () => Date },
 		requestDescription: '',
 		requestAdminSolved: [] as string[]
 	});
@@ -383,10 +383,10 @@ const RequestPage = ({ params, data }: { params: { id: string }; data: any }) =>
 			console.log('Date d\'intervention mise à jour avec succès dans Firestore');
 
 			// Mettre à jour l'état avec la nouvelle date
-			setFormData(prevFormData => ({
-				...prevFormData,
-				interventionDate: timestamp.toString() // Convertir en chaîne de caractères
-			}));
+			// setFormData(prevFormData => ({
+			//	...prevFormData,
+			//	interventionDate: timestamp.toString() // Convertir en chaîne de caractères
+			// }));
 		} catch (error) {
 			console.error('Erreur lors de la mise à jour de la date d\'intervention dans Firestore:', error);
 		}
@@ -635,33 +635,10 @@ const RequestPage = ({ params, data }: { params: { id: string }; data: any }) =>
 														<div className='grid gap-3'>
 														<CardTitle>
 															Date d&apos;Intervention {" "}
-															<button className="ml-auto sm:ml-0 bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
-																<span className="absolute inset-0 overflow-hidden rounded-full">
-																	<span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
-																</span>
-																<div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
-																	<span>
-																		{formData.interventionDate &&
-																			new Date(parseInt(formData.interventionDate)).toLocaleString()}
-																	</span>
-																	<svg
-																	fill="none"
-																	height="16"
-																	viewBox="0 0 24 24"
-																	width="16"
-																	xmlns="http://www.w3.org/2000/svg"
-																	>
-																	<path
-																		d="M10.75 8.75L14.25 12L10.75 15.25"
-																		stroke="currentColor"
-																		strokeLinecap="round"
-																		strokeLinejoin="round"
-																		strokeWidth="1.5"
-																	/>
-																	</svg>
-																</div>
-																<span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-															</button>							
+															<Badge>
+																	{formData.createdAt?.toDate()?.toLocaleString() || ''}
+															</Badge>
+																						
 														</CardTitle>
 															<Popover>
 																<PopoverTrigger asChild>
@@ -682,13 +659,13 @@ const RequestPage = ({ params, data }: { params: { id: string }; data: any }) =>
 																		selected={date}
 																		onSelect={(selectedDate) => {
 																			setDate(selectedDate);
-																			if (selectedDate) {
-																				updateDateInFirestore(selectedDate);
-																				setFormData((prevFormData) => ({
-																					...prevFormData,
-																					interventionDate: selectedDate.getTime().toString() // Convertir en chaîne de caractères
-																				}));
-																			}
+																		//	if (selectedDate) {
+																		//		updateDateInFirestore(selectedDate);
+																		//		setFormData((prevFormData) => ({
+																		//			...prevFormData,
+																		//			interventionDate: selectedDate.getTime().toString() // Convertir en chaîne de caractères
+																		//		}));
+																		//	}
 																		}}
 																		initialFocus
 																	/>
